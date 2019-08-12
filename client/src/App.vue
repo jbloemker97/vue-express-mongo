50908443056ebab81c5ce977d86b67f23d4fe269
Jacob Bloemker <jbloemker97@gmail.com>
2019-08-11 20:11:43 -0400

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PostComponent/>
+  </div>
+</template>
+
+<script>
+import PostComponent from './components/PostComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PostComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
